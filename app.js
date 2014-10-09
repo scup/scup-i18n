19f8ca265e7bb9482a2990285c783387d87cc65a
Willian R. Sabião <willianjedai@gmail.com>
2014-10-08 23:25:20 -0300

    Separating i18n API from tool terminal

--- app.js
+++ app.js
@@ -1,4 +1,102 @@
 #!/usr/bin/env node
 
-var setup = new require('./index');
-setup.start();
+var I18N = require('./index'),
+  Ordine = require('ordine'),
+  inquirer = require('inquirer'),
+  questions = {};
+
+questions.project = {
+  type:'input',
+  name:'project',
+  message:'Project name'
+};
+
+questions.user = {
+  type:'input',
+  name:'username',
+  message:'Transifex Username'
+};
+
+questions.password = {
+  type:'password',
+  name:'password',
+  message:'Transifex Password'
+};
+
+questions.language = {
+  type:'checkbox',
+  name:'languages',
+  message:'Select the language :',
+};
+
+questions.resource = {
+  type:'checkbox',
+  name:'resources',
+  message:'Select the resources :',
+};
+
+
+var flush = new Ordine(function(){});
+var user, pass, proj;
+
+flush.enqueue(function(){
+  inquirer.prompt(questions.project,function(data){
+    proj = data.project;
+    flush.next()
+  });
+});
+
+flush.enqueue(function(){
+  inquirer.prompt(questions.user,function(data){
+    user = data.username;
+    flush.next()
+  });
+},true);
+
+flush.enqueue(function(){
+  inquirer.prompt(questions.password,function(data){
+    pass = data.password;
+    I18N.authenticate(user, pass, proj);
+    flush.next();
+  });
+},true);
+
+
+flush.enqueue(function() {
+  questions.language.choices =  []
+  I18N.getLangs(function(data) {
+    
+    data.forEach(function(choice,i) {
+      questions.language.choices.push(choice.language_code);
+    });
+
+    inquirer.prompt(questions.language, function(data){
+      I18N.setLangs(data.languages);
+      flush.next();
+    });
+
+  });
+},true);
+
+flush.enqueue(function(){
+
+  I18N.getResources(function (data) {
+    questions.resource.choices = [];
+    data.forEach(function(option,i){
+      questions.resource.choices.push(option.slug)
+    });
+    inquirer.prompt(questions.resource,function(data){
+      I18N.setResources(data.resources);
+      flush.next();
+    });
+  });
+
+},true);
+
+flush.enqueue(function(){
+  I18N.translations('teste.json', function () {
+    flush.next();
+  });
+},true);
+
+flush.run();
--- index.js
+++ index.js
@@ -1,151 +1,56 @@
 #!/usr/bin/env node
 
 var _args = require('simple-args'),
-  transifex = new require('transifexjs'),
-  inquirer = require('inquirer'),
-  Ordine = require('ordine');
+  transifex = new require('transifexjs');
 
 module.exports = (function(){
   'use Strict'
 
-  var questions = [];
-
-  questions.push({
-    type:'input',
-    name:'username',
-    message:'Transifex Username'
-  })
-
-  questions.push({
-    type:'password',
-    name:'password',
-    message:'Transifex Password'
-  })
-
-  questions.push({
-    type:'checkbox',
-    name:'resources',
-    message:'Select the resources :',
-  })
-
-  questions.push({
-    type:'checkbox',
-    name:'languages',
-    message:'Select the language :',
-  })
-
-
-  var I18N = this;
-  var flush = new Ordine(function(){console.log('fim')})
-  flush.enqueue(function(){
-    inquirer.prompt(questions[0],function(username){
-      I18N.username = username['username'];
-      flush.next()
-    })
-  })
-
-  flush.enqueue(function(){
-    inquirer.prompt(questions[1],function(password){
-      I18N.password = password['password'];
-      authenticate()
-    })
-  },true)
-
-
-
-
-
-  flush.enqueue(function(){
-    langs()
-  },true)
-
-
-  flush.enqueue(function(){
-    resources()
-  },true)
-
-
-  flush.enqueue(function(){
-    translations()
-  },true)
-
-
-
-  if (_args.hasOwnProperty('usr')){
-    console.log('defined usr')
-    I18N.username = _args['usr'];
-  }
-
-  if (_args.hasOwnProperty('pass')){
-    console.log('defined pass')
-    I18N.password = _args['pass'];
-  }
-
-
-
-
-  function authenticate(){
-    transifex = transifex.login(I18N.username,I18N.password)
-    .setProject('site-5');
-    flush.next();
-  }
-
-  function resources(){
-    transifex.resources(function(x){
-      x = JSON.parse(x)
-      questions[2].choices = [];
-      x.forEach(function(option,i){
-        questions[2].choices.push(option.slug)
-      })
-      inquirer.prompt(questions[2],function(resources){
-        transifex.setResource(resources['resources'][0]);
-        flush.next();
-      })
-    })
-  }
-
-  var langs = function(){
-    transifex.languages(function(x){
-      questions[3].choices =  []
-      JSON.parse(x).forEach(function(choice,i){
-        questions[3].choices.push(choice.language_code);
-      })
-      inquirer.prompt(questions[3],function(languages){
-        I18N.languages = languages;
-        transifex.setLang(languages['languages'][0])
-        flush.next();
-      })
-    })
-  }
-
-
-  var translations = function(){
-    transifex.translation(function(x){
-      x = JSON.parse(JSON.parse(x)['content']);
-
-      require('fs').writeFile("teste.json", JSON.stringify(x), function(err) {
-        if(err) {
-          console.log(err);
-        } else {
-          console.log("The file was saved!");
-        }
-      }); 
-
-      flush.next();
-    })
-  }
-
-
-  this.applyTranslations = function(){
-
-
-
-  }
-
   return {
-    start : function(){
-      console.log('run');
-      flush.run()
+    authenticate: function(user, pass, proj) {
+      transifex = transifex.login(user,pass)
+      .setProject(proj);
+    },
+    getResources: function (cb) {
+      transifex.resources(function(x){
+        if( typeof cb === 'function' ) {
+          cb(JSON.parse(x));
+        }
+      });
+    },
+    
+    setResources: transifex.setResource,
+
+    getLangs: function(cb) {
+      transifex.languages(function(x){
+        if( typeof cb === 'function' ) {
+          cb(JSON.parse(x));
+        }
+      });
+    },
+
+    setLangs: transifex.setLang,
+
+    translations: function(path, cb) {
+      if(!path || typeof path !== 'string') {
+        throw new Error('Path undefined;');
+        return;
+      }
+
+      transifex.translation(function(x){
+        x = JSON.parse(JSON.parse(x)['content']);
+
+        require('fs').writeFile(path, JSON.stringify(x), function(err) {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log("The file was saved!");
+          }
+        }); 
+        if (typeof cb === 'function')
+          cb();
+      })
     }
-  }
-})()
+  };
+
+})();
